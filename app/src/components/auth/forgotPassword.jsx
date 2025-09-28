77eb37b02b8ec1880efb6bb641dfa98461bbde74
Mithun Kumar <jeetukumargupta53@gmail.com>
2025-09-28 16:36:09 +0530

    add content in readme

--- app/src/components/auth/forgotPassword.jsx
+++ app/src/components/auth/forgotPassword.jsx
@@ -1,8 +1,72 @@
+import { useState } from "react";
+import axios from "axios";
+import { Cookies } from "react-cookie"
+import { useNavigate } from "react-router-dom";
+function ForgotPassword() {
+  const [submit , setsubmit]= useState(false)
+  const [email, setEmail] = useState("")
+  const navigate = useNavigate();
+  async function handleResetPassowrd(e) {
+    
+    e.preventDefault()
+    try {
+      setsubmit(true)
+      if (!email.trim()) {
+        alert("fill email properly")
+        return;
+      } 
+      const { data } = await axios.post(`${import.meta.env.VITE_URL}/user/forgotPassword`, { email })
+      if (data?.message === "opt is send to your email account complete next step") {
+      localStorage.setItem("forgotPasswordEmail", email)
+      navigate("/otpverifyAfterForgetPassword", { state: { email } }) 
+      }         
+    } catch (error) {
+      console.log(error)
+       alert(error?.response?.data)
+    } finally {
+   
+      setEmail("")
+      setsubmit(false)   
+    }
+  }
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-50 px-4">
+      <form className="w-full max-w-md bg-white rounded-xl shadow-md p-6 space-y-4" onSubmit={handleResetPassowrd}>
+        <h2 className="text-xl font-bold text-center text-gray-800">  Retrive  your Account </h2>
 
+        <div>
+          <label id="email" className="block text-sm font-medium text-gray-700">
+            Email Address
+          </label>
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required= {true}
+            type="email"
+            id="email"
+            placeholder="Enter your email"
+            className="mt-1 w-full px-3 py-2 border rounded-md focus:outline-none focus:ring focus:ring-blue-200"
+          />
+        </div>
 
-export default function ForgotPassword(){
-     return (<>
-       forgot password 
-     </>)
+<button
+  disabled={submit}
+  type="submit"
+  className={`w-full py-2 rounded-md transition font-medium ${
+    submit ? "opacity-50 cursor-not-allowed bg-blue-400 text-white" : "bg-blue-600 text-white hover:bg-blue-700"
+  }`}
+>
+  Retrieve Account
+</button>
+
+      </form>
+    </div>
+  );
 }
+
+export default ForgotPassword
+
+  
+
+
 
---
+++ app/src/components/auth/verifyOtp.jsx
@@ -0,0 +1,122 @@
+import axios from 'axios';
+import React, { useState } from 'react';
+import { showEmojiAlert } from 'alert_popup_emoji';
+import { Link , useNavigate } from 'react-router-dom';
+import { useCookies } from 'react-cookie';
+ 
+const OtpVerify = () => {
+  
+  const [otp, setOtp] = useState(new Array(6).fill(""));
+  const [email, setEmail] = useState("");
+  const [isLoading, setIsLoading] = useState(false)
+  const [error , setError] = useState("")
+  const navigate = useNavigate();
+  const [cookies, setcookie, removecookie] = useCookies(['refreshToken']);
+  const handleChange = (value, index) => {
+    const updatedOtp = [...otp];
+    updatedOtp[index] = value;
+    setOtp(updatedOtp);
+  };
+   function handleGoBack(e) { 
+     e.preventDefault();
+     setIsLoading(false);
+     setEmail("");
+     setOtp(new Array(6).fill(""))
+     setError("")
+    
+   }
+
+  async function verify(e) {
+    e.preventDefault();
+    setIsLoading(true);
+    try {
+      const { data } = await axios.post(`${import.meta.env.VITE_URL}/user/verifyotp`, {
+        email,
+        otp : Number(otp.join(""))
+      });
+      console.log(data)
+      if (data?.message === "Account verified successfully") {
+        setcookie('refreshToken', data, {
+          path: '/',
+          maxAge: 7 * 24 * 60 * 60,
+       
+        });
+      
+        navigate('/');
+      }
+    } catch (err) {
+       showEmojiAlert({message : err?.response?.data  , type : false , sound : true})
+      console.log(err);
+    }
+    finally { 
+           setIsLoading(false)
+    }
+  }
+//   if(error){
+//   return (
+//     <div className="flex justify-center items-center h-screen w-screen">
+//      <div>
+//       <h1 className="font-semibold text-5xl text-red-600">{ error || "Something went wrong"}</h1>
+//         <button className="text-black text-xl hover:underline  font-extrabold " onClick={handleGoBack}> back</button>   
+//      </div>
+//     </div>
+//   ); 
+// }
+
+
+  return (
+    <>
+      {
+      isLoading  === true?
+      <div className="flex  justify-center  items-center  w-screen h-screen">
+      <h1 className="text-5xl font-bold text-green-600">
+      loading<span className="animate-ping">...</span>
+      </h1>
+      </div>
+      
+      : <div className="flex items-center justify-center min-h-screen bg-gray-100 font-sans">
+      <h1 className="fixed top-0 left-0 w-full text-center bg-yellow-100 text-amber-600 font-semibold text-sm md:text-base py-2 shadow-md animate-pulse z-50">
+     📩 Since i am using  some free services, please check your emai <span className="font-bold">Spam</span> folder for the verification email.
+     </h1>      
+      <div className="text-center">
+        <h2 className="text-2xl font-semibold mb-6">Enter OTP</h2>
+
+        <input
+          type="email"
+          placeholder="Enter your email"
+          className="mb-4 px-4 py-2 w-64 border border-blue-400 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 shadow-sm"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <div className="flex justify-center gap-2 mb-4">
+          {otp.map((digit, i) => (
+            <input
+              key={i}
+              type="text"
+              maxLength="1"
+              value={digit}
+              onChange={(e) => handleChange(e.target.value, i)}
+              className="w-10 h-10 text-center border border-gray-400 rounded text-lg"
+            />
+          ))}
+        </div>
+
+        <button
+          className="bg-blue-500 text-white px-5 py-2 rounded hover:bg-blue-600"
+          onClick={verify}
+        >
+          Verify
+        </button>
+        <Link  to={"/resendOtp"}  className='text-xl  text-red-400'> resend otp</Link>
+      </div>
+    </div>
+      }
+    </>
+  );
+};
+
+export default OtpVerify;
+
+
+
